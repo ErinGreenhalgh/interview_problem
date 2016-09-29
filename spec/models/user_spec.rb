9644046e22a7671fc04f37b493a0d40c61f268d4
Erin Greenhalgh <erin.greenhalgh0113@gmail.com>
2016-09-29 07:15:17 -0600

    change assertions in user model spec to check if the user is valid rather than checking to see if it saves to the database

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,25 +9,25 @@ RSpec.describe User, :type => :model do
   context "with correct values" do
     scenario "validates social security number and email" do
       user = User.new(first_name: "steve", last_name: "jobs", email: "steve@apple.com", social_security_number: "012345678")
-      expect(user.save).to be true
+      expect(user.valid?).to be true
     end
   end
 
   context "with incorrect values" do
     scenario "does not validate social security number that is the incorrect length" do
       user = User.new(first_name: "steve", last_name: "jobs", email: "steve@apple.com", social_security_number: "01234")
-      expect(user.save).to be false
+      expect(user.valid?).to be false
     end
 
     scenario "does not validate social security number that has anythign but numbers" do
       user = User.new(first_name: "steve", last_name: "jobs", email: "steve@apple.com", social_security_number: "01234abcd")
 
-      expect(user.save).to be false
+      expect(user.valid?).to be false
     end
 
     scenario "does not validate incorrectly formatted email" do
       user = User.new(first_name: "steve", last_name: "jobs", email: "bad_email", social_security_number: "012345678")
-      expect(user.save).to be false
+      expect(user.valid?).to be false
     end
   end
 end
