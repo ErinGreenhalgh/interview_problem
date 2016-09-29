2c0c797f1e620ba34d3082273b287ea5a5dcde08
Erin Greenhalgh <erin.greenhalgh0113@gmail.com>
2016-09-28 21:58:22 -0600

    add sad path test to handle unsuccessful create action and add logic in controller for error handling - also tweak successful create to send a 201 status code

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,10 +1,21 @@
 class Api::V1::UsersController < ApplicationController
   def create
-    render json: User.create(user_params)
+    user = User.new(user_params)
+    if user.save
+      render status: 201, json: user
+    else
+      render_error(user)
+    end
   end
 
   private
   def user_params
     params.require(:user).permit(:first_name, :last_name, :email, :social_security_number)
+  end
+
+  def render_error(user)
+    render status: 400, json: {
+      message: user.errors.full_messages.join(". ")
+    }
   end
 end
--- spec/api/v1/users_controller_spec.rb
+++ spec/api/v1/users_controller_spec.rb
@@ -8,13 +8,25 @@ RSpec.describe "api::v1::users_controller" do
                      email:      "steve@apple.com",
                      social_security_number: "123456789" }
       post "/api/v1/users", user: user_params
-      expect(response).to be_success #check that 200 is the appropriate response for create
+      expect(response.status).to be 201
       data = JSON.parse(response.body)
       expect(data.keys).to eq(["id", "first_name", "last_name", "email"])
-      epext(data.id).to             eq 1
+      expect(data["id"]).to             eq 1
       expect(data["first_name"]).to eq user_params[:first_name]
       expect(data["last_name"]).to  eq user_params[:last_name]
       expect(data["email"]).to      eq user_params[:email]
+    end
+  end
+
+  context "with invalid params" do
+    scenario "renders an error message" do
+      bad_params =  {email:      "bad email",
+                     social_security_number: "1234" }
+      error_message = "First name can't be blank. Last name can't be blank. Social security number is the wrong length (should be 9 characters). Email is invalid"
+      post '/api/v1/users', user: bad_params
+      expect(response.status).to eq 400
+      data = JSON.parse(response.body)
+      expect(data["message"]).to eq error_message
     end
   end
 end
