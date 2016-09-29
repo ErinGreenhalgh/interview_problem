7197148b38ee244736ebfc7d8dc81801579c9840
Erin Greenhalgh <erin.greenhalgh0113@gmail.com>
2016-09-28 19:15:14 -0600

    set up versioned api with a create action - need to set up a serializer to get the right json format as per spec

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,10 @@
+class Api::V1::UsersController < ApplicationController
+  def create
+    render json: User.create(user_params)
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :social_security_number)
+  end
+end
